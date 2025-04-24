0933f4fa7438bf5554cddecc54ef5311a4ff4ba7
Mike <ban.mihaiconstantin@gmail.com>
2025-04-24 12:30:52 +0300

    Model -> Domain

--- VSFrontendBackend.Server/Utils/GameDataGenerator.cs
+++ VSFrontendBackend.Server/Utils/GameDataGenerator.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using VSFrontendBackend.Server.Models;
+using VSFrontendBackend.Server.Domain;
 
 namespace VSFrontendBackend.Server.Utils
 {
