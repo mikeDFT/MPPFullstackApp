20159b1817c95d5cf953e3385c6fd87cd79309fa
Mike <ban.mihaiconstantin@gmail.com>
2025-04-26 11:11:44 +0300

    Update StatisticsForPrice.jsx

--- vsfrontendbackend.client/src/utils/StatisticsForPrice.jsx
+++ vsfrontendbackend.client/src/utils/StatisticsForPrice.jsx
@@ -3,8 +3,6 @@ export function StatisticsForPrice(gamesInfo) {
     var sortedGames = [...gamesInfo]
     sortedGames.sort((a, b) => {return a.Price - b.Price;})
 
-    console.log("SortedStats", sortedGames)
-
     var nrGames = sortedGames.length / 3
     var out = {}
     for (var i = 0; i < sortedGames.length; i++) {
@@ -17,8 +15,6 @@ export function StatisticsForPrice(gamesInfo) {
         else
             out[game.Id] = "$$$";
     }
-
-    console.log("StatisticsForPrice", out)
 
     return out
 }
